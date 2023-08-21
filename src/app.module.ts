a77e05786dd1a75e72bd862f17f52c4aabaf70f8
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-08-21 08:24:42 -0300

    created read and create crud api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModel } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModel],
   controllers: [AppController],
   providers: [AppService],
   exports: [AppService],
